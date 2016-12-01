95431ea3c0b73ee53842e1577bf6534ac23e418d
nigulasizhaosi <1036971959@qq.com>
2016-12-01 14:40:00 +0800

    -a

--- js/connect.js
+++ js/connect.js
@@ -13,18 +13,41 @@
 				$.ajax({
 					url : '/login',
 					type : "post",
-					data : "username=" +username+"&"+"password="+password
-
+					data : "username=" +username+"&"+"password="+password,
+					success : function(json){
+						if(json.statu==true)
+							return true;
+						else
+							console.log('error!');
+					}
+				})
+			})
+			return true;
+		})();
+	},
+	singBody : function(){
+		var div = view.singBody;
+		return (function(){
+			$('body').append(div);
+			$("#sing").click(function(){
+				var sUserName,sPassWord,againPassWord,codeComfirm;
+				sUserName = $("#sUserName").val();
+				sPassWord = $("#sPassWord").val();
+				againPassWord = $("#againPassWord").val();
+				codeComfirm = $("#codeComfirm").val();
+				$.ajax({
+					url : "/sing",
+					type : "post",
+					data : "sUserName=" + sUserName + "&sPassWord = "+sPassWord,
+					success : function(json){
+						
+					}
 				})
 			})
 		})();
-		
-
-	},
-	sing : function(){
 	}
 
 	}
-	connect.login();
+	connect.singBody();
 	window.connect = connect;
 })(window)
